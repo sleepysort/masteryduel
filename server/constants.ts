1c5d2238214dfd6def0b1daac2bc68413e01657b
Ubuntu <sleepysort@sleepybox.x0jzlfpjoyve3h5hyesl4dfpuf.dx.internal.cloudapp.net>
2016-05-09 17:15:46 +0000

    removed old api key

--- server/constants.ts
+++ server/constants.ts
@@ -10,7 +10,6 @@ export const SERVER_ROOT: string = path.resolve(PROJ_ROOT, 'server');
 
 
 export const LOL_API_URL: string = 'https://na.api.pvp.net';
-export const LOL_API_KEY: string = 'api_key=c0e7cfb6-55b2-45f3-9ed6-f9eaacda4fb9';
 
 export const MAX_PLAYERS: number = 2;
 export const NEXUS_STARTING_HEALTH: number = 5;
